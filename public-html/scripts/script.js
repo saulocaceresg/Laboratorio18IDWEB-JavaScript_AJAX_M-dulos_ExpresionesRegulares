ce9a7aacf855b475f1634e899c2da8a688db3f8a
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-28 13:53:24 -0500

    Commit 3: segundo ejercicio (4.) resuelto -> fetch con async/await

--- public-html/scripts/script.js
+++ public-html/scripts/script.js
@@ -13,4 +13,25 @@ function cargarUsuario() {
     .catch(error => console.error(error));
 }
 
-cargarUsuario(); // Se llama a la función
+// cargarUsuario(); // Se llama a la función
+
+// 4.	Usar fetch con async/await para leer datos de una API: crear una función cargarUsuario(), consultar al API jsonplaceholder sobre el usuario 10, convertir la respuesta a JSON y mostrar en consola el name, username y el email del usuario
+console.log("+-----------------------------+\nEjercicio 2 (4.)\n\"Fetch con async/await\"");
+
+async function cargarUsuario2() {
+    try {
+        const respuesta = await fetch ("https://jsonplaceholder.typicode.com/users/10"); // se vincula con el API
+
+        // Para atrapar un error en el url
+        if (!respuesta.ok) {
+            throw new Error ("HTTP ERROR:", respuesta.status);
+        }
+        
+        const data = await respuesta.json(); // Se convierte a JSON
+        console.log("Datos:\nName:", data.name, "\nUsername:", data.username, "\nEmail:", data.email); // Se imprime
+    } catch (e) {
+        console.error("Error:", e);
+    }
+}
+
+cargarUsuario2();
